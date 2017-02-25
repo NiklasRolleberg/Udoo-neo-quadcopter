85a260c87c72f7871cf978f470e1c16128c6de02
niklasrolleberg <niklasr93@gmail.com>
2017-02-25 15:28:06 +0100

    changes for new communication protocol

--- A9/A9.cpp
+++ A9/A9.cpp
@@ -5,10 +5,13 @@ A9::A9()
 {
   std::cout << "A9 constructor" << std::endl;
 
+  /*Open serial port to M4*/
   serialport_M4.setSerialPort("/dev/ttyMCC",115200);
   serialport_M4.AddObserver(*this);
   serialport_M4.start();
 
+  /*Open serial port to gps (does not work.. The seraial port seams to be able
+  /* to send data but not receive)*/
   //serialport_GPS.setSerialPort("/dev/ttymxc5",9600);
   //serialport_GPS.AddObserver(*this);
   //serialport_GPS.start();
@@ -41,17 +44,37 @@ void A9::update()
   }
   if(network.hasChanged())
   {
-    
+
     std::string s = network.getMessage();
     /* set pulse length */
-    
+
+    std::cout << "network message received: " << '"' << s << '"' << std::endl;
+    //std::cout << "substr: " << s.substr(0,2) << std::endl;
+    if(s.substr(0,2) == ":1")
+    {
+      //Message is ment for A9
+    }
+    else if(s.substr(0,2) == ":2")
+    {
+      //Message is ment for M4
+      //std::cout << "message to M4" << std::endl;
+
+      if(s[s.length()-1] == ';') //message is probably OK
+      {
+        std::string toSend = s.substr(2,s.length()-3);
+        //std::cout << "message to send: " << toSend << std::endl;
+        serialport_M4.send(toSend);
+      }
+    }
+
+    /*
     std::cout << "From Android: " << s << std::endl;
     std::string toSend = "$QCPUL";
     toSend += s.substr (10,20);
     toSend += ",*00\n";
     std::cout << "toSend: " << toSend << std::endl;
     serialport_M4.send(toSend);
-    
+    */
     /*
     std::cout << "From Android: " << s << std::endl;
     std::string toSend = "$QCSTA,0,0,0,";
